01ce7aa84a77b30857e7931af2f7a1255a1d6d68
nexonsoftware01 <nexonsoftware01@gmail.com>
2026-05-14 11:18:19 +0530

    dummy commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function usePath() {
     const onPop = () => {
       setPath(window.location.pathname);
       window.scrollTo({ top: 0, behavior: 'auto' });
+      
     };
     window.addEventListener('popstate', onPop);
     return () => window.removeEventListener('popstate', onPop);
